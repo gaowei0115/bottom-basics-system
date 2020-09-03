37b6d435d109963a26e3bfe0d73eee865e64aed7
gao_wei0115@sina.com <gao_wei0115@sina.com>
2020-09-03 22:19:49 +0800

    基于Netty框架 模式实现简单基于RPC协议

---
+++ basics-socket-system/netty-system/netty-rpc/netty-rpc-core/src/main/java/com/mmc/netty/rpc/core/App.java
@@ -0,0 +1,20 @@
+package com.mmc.netty.rpc.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @packageName：com.mmc.netty.rpc.core
+ * @desrciption:
+ * @author: GW
+ * @date： 2020/9/2 22:29
+ * @history: (version) author date desc
+ */
+public class App {
+
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.debug("test logger");
+    }
+}
